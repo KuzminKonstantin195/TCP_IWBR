8635b51a84ef04090e658f06f69f1f2587e1fdaf
Konstantin Kuzmin <konstantinkuzmin195@gmail.com>
2025-02-10 22:01:09 +0700

    initial

---
+++ all_headers.h
@@ -0,0 +1,20 @@
+#ifndef ALL_HEADERS_H
+#define ALL_HEADERS_H
+
+#include <QObject>
+
+#include <QTcpSocket>
+#include <QTcpServer>
+#include <QDataStream>
+
+#include <QByteArray>
+#include <QString>
+#include <QVector>
+
+//#include <iostream>
+#include <cstdio>
+#include <memory>
+#include <thread>
+#include <mutex>
+
+#endif // ALL_HEADERS_H
